89c84af371e1cac7d0312891720b1d9f0e81600a
invi1998 <2267661826@qq.com>
2024-09-02 13:25:33 +0800

    创建一个基础Tab布局

--- Plugins/SuperManager/Source/SuperManager/Private/SlateWidgets/AdvanceDeletionWidget.cpp
+++ Plugins/SuperManager/Source/SuperManager/Private/SlateWidgets/AdvanceDeletionWidget.cpp
@@ -7,9 +7,23 @@ void SAdvanceDeletionTab::Construct(const FArguments& InArgs)
 {
 	bCanSupportFocus = true;	// 设置焦点
 
+	FSlateFontInfo TitleFontInfo = FCoreStyle::Get().GetFontStyle("EmbossedText");
+	TitleFontInfo.Size = 24;
+
 	ChildSlot
 		[
-			SNew(STextBlock)
-				.Text(FText::FromString(InArgs._TestString))
+			// 垂直布局
+			SNew(SVerticalBox)
+
+				// 第一行
+				+ SVerticalBox::Slot()
+				.AutoHeight()
+				[
+					SNew(STextBlock)
+						.Text(FText::FromString("Advance Deletion"))
+						.Font(TitleFontInfo)
+						.Justification(ETextJustify::Center)
+						.ColorAndOpacity(FLinearColor::White)
+				]
 		];
 }
