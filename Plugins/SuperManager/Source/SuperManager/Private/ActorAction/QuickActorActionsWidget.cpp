ba3471dab854f4353e06802d4366220ebaf51364
invi1998 <2267661826@qq.com>
2024-09-08 20:29:47 +0800

    Actor批量选择

--- Plugins/SuperManager/Source/SuperManager/Private/ActorAction/QuickActorActionsWidget.cpp
+++ Plugins/SuperManager/Source/SuperManager/Private/ActorAction/QuickActorActionsWidget.cpp
@@ -3,6 +3,51 @@
 
 #include "ActorAction/QuickActorActionsWidget.h"
 #include "Subsystems/EditorActorSubsystem.h"
+#include "DebugHeader.h"
+
+void UQuickActorActionsWidget::SelectAllActorsWithSimilarName()
+{
+	if (!GetEditorActorSubsystem()) return;
+
+	TArray<AActor*> SelectedActors = EditorActorSubsystem->GetSelectedLevelActors();		// 获取选中的Actor
+	uint32 SelectedCount = 0;
+
+	if (SelectedActors.Num() <= 0)
+	{
+		SM_Debug::ShowNotifyInfo(FText::FromString("No actor selected!"), FText::FromString("Warning"));
+		return;
+	}
+
+	if (SelectedActors.Num() > 1)
+	{
+		SM_Debug::ShowNotifyInfo(FText::FromString("Only one actor can be selected!"), FText::FromString("Warning"));
+		return;
+	}
+
+	const FString ActorName = SelectedActors[0]->GetActorLabel();		// 获取选中的Actor的名称
+	const FString NameToSearch = ActorName.LeftChop(4);			// 去掉最后四位字符
+
+	TArray<AActor*> AllActors = EditorActorSubsystem->GetAllLevelActors();	// 获取所有的Actor
+
+	for (AActor* Actor : AllActors)
+	{
+		if (Actor && Actor->GetActorLabel().Contains(NameToSearch))
+		{
+			EditorActorSubsystem->SetActorSelectionState(Actor, true);	// 选中Actor
+			SelectedCount++;
+		}
+	}
+
+	if (SelectedCount > 0)
+	{
+		SM_Debug::ShowNotifyInfo(FText::FromString(FString::Printf(TEXT("Selected %d actors!"), SelectedCount)), FText::FromString("Success"));
+	}
+	else
+	{
+		SM_Debug::ShowNotifyInfo(FText::FromString("No actor selected!"), FText::FromString("Warning"));
+	}
+
+}
 
 bool UQuickActorActionsWidget::GetEditorActorSubsystem()
 {
--- Plugins/SuperManager/Source/SuperManager/Public/ActorAction/QuickActorActionsWidget.h
+++ Plugins/SuperManager/Source/SuperManager/Public/ActorAction/QuickActorActionsWidget.h
@@ -14,6 +14,14 @@ class SUPERMANAGER_API UQuickActorActionsWidget : public UEditorUtilityWidget
 {
 	GENERATED_BODY()
 
+public:
+	UFUNCTION(BlueprintCallable)
+	void SelectAllActorsWithSimilarName();	// 选择所有名称相似的Actor
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ActorBatchSelection")
+	TEnumAsByte<ESearchCase::Type> SelectionMode = ESearchCase::IgnoreCase;	// 选择模式:默认忽略大小写
+
+
 private:
 	UPROPERTY()
 	class UEditorActorSubsystem* EditorActorSubsystem;
